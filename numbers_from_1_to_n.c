3c89114b925b16153609a6fc2aff24048bae10e8
BTSManideep <btsmanidheep@gmail.com>
2024-12-09 11:47:31 +0530

    Create numbers_from_1_to_n.c

---
+++ numbers_from_1_to_n.c
@@ -0,0 +1,12 @@
+//c program to print all the numbers from 1 to N
+#include <stdio.h>
+void main() 
+{
+    int i,n;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    for(i=1;i<=n;++i)
+    {
+        printf("%d\n",i);
+    }
+}
