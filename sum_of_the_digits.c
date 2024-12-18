46ebcc8506d2a7553db74bbebc5bc83aa2c55666
BTSManideep <btsmanidheep@gmail.com>
2024-12-18 15:20:03 +0530

    sum_of_the_digits.c

---
+++ sum_of_the_digits.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+   int n,sum=0,rem;
+   printf("enter the value of n:");
+   scanf("%d",&n);
+   while(n>0)
+   {
+       rem=n%10;
+       sum=sum+rem;
+       n=n/10;
+   }
+   printf("sum is %d",sum);
+}
