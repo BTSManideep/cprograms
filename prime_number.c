4bf4a689c8ff54425fd6fc5abeca01b9c8932fd8
BTSManideep <btsmanidheep@gmail.com>
2024-12-23 11:44:50 +0530

    prime_number.c

---
+++ prime_number.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+int main() 
+{
+    int i,n,count=0;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            count++;
+        }
+    }
+    if(count==2)
+    {
+        printf("it is a prime number...");
+    }
+    else
+    {
+        printf("it is not a prime number...");
+    }
+}
